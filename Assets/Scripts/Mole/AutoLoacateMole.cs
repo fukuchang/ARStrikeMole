4867b3cacd068f4265b0dacb88f1e7c61722dec6
福田　晃久 <basketplayer21.aki@gmail.com>
2020-08-31 19:56:48 +0900

    もぐらの生成方法がしっかりアルゴリズム通りに動いているかと見直し

--- Assets/Scripts/Mole/AutoLoacateMole.cs
+++ Assets/Scripts/Mole/AutoLoacateMole.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
 public class AutoLoacateMole : LocateMole
 {
-
+    [SerializeField] private Text recogText;
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +18,10 @@ public class AutoLoacateMole : LocateMole
     // Update is called once per frame
     void Update()
     {
+        var vec = RandomPos();
         if (raycastManager.Raycast(RandomPos(), raycastHits, TrackableType.All) && isLocate)
         {
+            recogText.text = vec.x.ToString();
             isLocate = false;
             StartCoroutine(LocateMole());
             StartCoroutine(GenerateMoleCoroutine(mole, raycastHits[0].pose, 2.0f));
@@ -40,20 +43,19 @@ public class AutoLoacateMole : LocateMole
         {
             case 0:
             case 1:
+                return new Vector3(0, Screen.height / 2 + range, 0);
             case 2:
             case 3:
-                return new Vector3(0, Screen.height / 2 + range, 0);
             case 4:
             case 5:
             case 6:
-            case 7:            
+            case 7:
                 return new Vector3(Screen.width, Screen.height / 2 + range, 0);
             case 8:
             case 9:
                 return centerPos;
             default:
                 return centerPos;
-                break;
         }
     }
 }
